2ec5f4de13a8e8a1d850b18ce0ccc821e68f512d
csalas <c.salas@yubico.com>
2022-08-18 10:07:50 -0500

    passkey autofill demo

---
+++ clients/web/react/src/Passkey/PasskeyLoginFlowPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import PasskeyLoginPage from "./PasskeyLoginPage";
+
+function PasskeyLoginFlowPage() {
+  return <PasskeyLoginPage />;
+}
+
+export default PasskeyLoginFlowPage;
